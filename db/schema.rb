74eb7a379b5c0728e6f371a93580fc3a80a9b5e6
Alex Hunley <ahunley@workwithopal.com>
2015-10-24 13:27:36 -0700

    Adding lessons

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024195015) do
+ActiveRecord::Schema.define(version: 20151024201638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lessons", force: :cascade do |t|
+    t.integer  "project_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "lessons", ["project_id"], name: "index_lessons_on_project_id", using: :btree
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
