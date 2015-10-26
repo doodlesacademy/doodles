e344951caef61b42d5db4c190c98c15c7dd8d51c
Alex Hunley <ahunley@workwithopal.com>
2015-10-26 08:33:21 -0700

    Cursory front-page styling

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,10 @@
+//$(function() {
+//});
+
+//$(window).scroll(function() {
+  //if($(document).scrollTop() > 0) {
+    //$('div#top-nav-bar').removeClass('large').addClass('small');
+  //} else {
+    //$('div#top-nav-bar').removeClass('small').addClass('large');
+  //}
+//});
