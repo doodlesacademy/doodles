b9cdd8abe39cc47ca8721b1a6b1fb83f58935b76
RJ Sheperd <rjsheperd@gmail.com>
2016-01-17 10:29:56 -0800

    Moved lessons and intro video to controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+  before_action :featured_lessons, only: [:index]
+
   def index
+    @intro_video_url = "https://player.vimeo.com/video/144787451"
   end
 
   def board
@@ -12,5 +15,27 @@ class HomeController < ApplicationController
   end
 
   def our_story
+  end
+
+  def featured_lessons
+    @featured_lessons = []
+    @featured_lessons.push({
+        title: "Symbol & Self",
+        video_id: "144787451",
+        image: "inspiration/symbol_self.jpg"
+      })
+
+    @featured_lessons.push({
+        title: "Superhero Lesson",
+        video_id: "140998518",
+        image: "inspiration/superhero.jpg"
+      })
+
+
+    @featured_lessons.push({
+        title: "Dream Home",
+        video_id: "144787451",
+        image: "inspiration/dream_home.jpg"
+      })
   end
 end
