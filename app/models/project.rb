eb6a8fe714f179a63e29e9c7e3d44eca47953d72
RJ Sheperd <rjsheperd@gmail.com>
2016-02-09 15:38:04 -0800

    Project slug generator

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,13 @@ class Project < ActiveRecord::Base
   has_many :supply_items, through: :lessons
 
   validates :title, presence: true
+  validates :slug, presence: true
   validates :level, inclusion: { in: %w(upper lower) }
+  before_save :generate_slug
+
+  private
+  def generate_slug
+    slug = self.title.downcase.gsub /[^a-z\s]+/, ''
+    self.slug = slug.strip.gsub /\s/, '-'
+  end
 end
