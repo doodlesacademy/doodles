07162cd8fd8e19e234340e23d5ee1236518daf90
RJ Sheperd <rjsheperd@gmail.com>
2016-03-01 06:20:49 -0800

    Standards. Who needs 'em

--- db/migrate/20160229091621_remove_sectionable_columns.rb
+++ db/migrate/20160229091621_remove_sectionable_columns.rb
@@ -4,11 +4,5 @@ class RemoveSectionableColumns < ActiveRecord::Migration
     remove_column :projects, :books_media
     remove_column :projects, :skills
     remove_column :projects, :photocopies
-
-    remove_column :standards, :common_core
-    remove_column :standards, :national_core
-    remove_column :standards, :art_elements
-    remove_column :standards, :art_principles
-    remove_column :standards, :cross_curricular
   end
 end
