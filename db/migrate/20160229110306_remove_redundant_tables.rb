81511c0210c4b04a3f98fbfe0998ca08bdd624d0
RJ Sheperd <rjsheperd@gmail.com>
2016-03-01 06:25:11 -0800

    Only drop tables if they exist

--- db/migrate/20160229110306_remove_redundant_tables.rb
+++ db/migrate/20160229110306_remove_redundant_tables.rb
@@ -1,10 +1,10 @@
 class RemoveRedundantTables < ActiveRecord::Migration
   def change
-    drop_table :admin_pages
-    drop_table :admin_projects
-    drop_table :admin_lessons
-    drop_table :admin_standards
-    drop_table :admin_members
-    drop_table :admin_galleries
+    drop_table :admin_pages, if_exists: true
+    drop_table :admin_projects, if_exists: true
+    drop_table :admin_lessons, if_exists: true
+    drop_table :admin_standards, if_exists: true
+    drop_table :admin_members, if_exists: true
+    drop_table :admin_galleries, if_exists: true
   end
 end
--- db/migrate/20160301124743_drop_standards_and_instruction_groups_and_instructions.rb
+++ db/migrate/20160301124743_drop_standards_and_instruction_groups_and_instructions.rb
@@ -1,7 +1,7 @@
 class DropStandardsAndInstructionGroupsAndInstructions < ActiveRecord::Migration
   def change
-    drop_table :standards
-    drop_table :instruction_groups
-    drop_table :instructions
+    drop_table :standards, if_exists: true
+    drop_table :instruction_groups, if_exists: true
+    drop_table :instructions, if_exists: true
   end
 end
