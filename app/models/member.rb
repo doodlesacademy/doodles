9738231ce6ffc4dd902c1f6dc70d23f63320206c
RJ Sheperd <rjsheperd@gmail.com>
2016-03-01 07:29:25 -0800

    Use Member Avatar

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ActiveRecord::Base
   enum role: [ :board, :volunteer ]
 
   has_attached_file :avatar, 
-    styles: { large: "900x900>", medium: "300x300>", thumb: "100x100>" }, 
+    styles: { large: "900x900>", medium: "300x300>", small: "200x200>", thumb: "100x100>" }, 
     default_url: "images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
