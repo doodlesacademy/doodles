30f0fde0104aa90a3f91cb41589622952546bf81
RJ Sheperd <rjsheperd@gmail.com>
2016-03-01 07:24:46 -0800

    S3 Host Name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :s3_region => ENV['S3_REGION']
+    :s3_region => ENV['S3_REGION'],
+    :s3_host_name => ENV['S3_HOST_NAME']
   }
 end
