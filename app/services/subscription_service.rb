5aa3d6e3298712d47b8869170ed304069477dc34
Alex Hunley <ahunley@workwithopal.com>
2016-04-05 20:39:05 -0700

    WIP

--- app/services/subscription_service.rb
+++ app/services/subscription_service.rb
@@ -1,15 +1,26 @@
 class SubscriptionService
-  def initialize(options)
-    @subscription_params = options[:subscription]
-    @email_address = @subscription_params[:email]
-    @api_request = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+  LIST_ID = ENV['MAILCHIMP_LIST_ID']
+  API_KEY = ENV['MAILCHIMP_API_KEY']
+
+  def initialize(subscription_params)
+    @subscription_params = subscription_params
+    @email_address = subscription_params[:email]
+    @subscription_id = subscription_params[:id]
+    @api_request = Gibbon::Request.new(api_key: API_KEY)
   end
 
   def subscribe
-    # @todo <alex> Better error-handling here obvs
+    # @todo <alex> Better error-handling/reporting here obvs
     return if @email_address.nil?
-    @api_request.lists(ENV['MAILCHIMP_LIST_ID']).create(create_params)
+    @api_request.lists(LIST_ID).members.create(create_params)
   end
+
+  def update
+    # @todo <alex> Better error-handling/reporting here obvs
+    return unless should_update?
+    @api_request.lists(LIST_ID).members(@subscription_id).update(update_params)
+  end
+
 
   private
   def create_params
@@ -19,5 +30,27 @@ class SubscriptionService
         status: "subscribed"
       }
     }
+  end
+
+  def update_params
+    {
+      body: {
+        email_address: @email_address,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscription_params[:first_name],
+          LNAME: subscription_params[:last_name]
+        }
+      }
+    }
+  end
+
+  def should_update?
+    has_name? && @subscription_id.present?
+  end
+
+  def has_name?
+    @subscription_params[:first_name].present? ||
+      @subscription_params[:last_name].present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get "page/:slug", to: 'home#page', as: 'pages'
   post 'feedback', to: 'feedback_messages#create'
   post 'subscribe', to: 'subscriptions#create'
+  post 'update_subscription', to: 'subscriptions#update'
+  get 'thanks', to: 'subscriptions#thanks'
 
   get 'admin', to: 'admin#index'
   namespace 'admin' do
