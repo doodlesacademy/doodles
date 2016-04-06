54316b17ecd6c06117269ad061d3ab92ed411c94
Alex Hunley <ahunley@workwithopal.com>
2016-04-06 01:32:05 -0700

    23 - adding contact requests controller and paraphernalia

---
+++ app/controllers/contact_requests_controller.rb
@@ -0,0 +1,22 @@
+class ContactRequestsController < ApplicationController
+  def new
+    @contact_request = ContactRequest.new
+  end
+
+  def create
+    @contact_request = ContactRequest.new(contact_request_params)
+    if @contact_request.save
+      render 'thanks'
+    else
+      render 'new', status: :unprocessable_entity, contact_request: @contact_request
+    end
+  end
+
+  private
+  def contact_request_params
+    params.require(:contact_request).permit(
+      :email_address, :first_name, :last_name, :location,
+      :involvement, :message, :subscribe
+    )
+  end
+end
