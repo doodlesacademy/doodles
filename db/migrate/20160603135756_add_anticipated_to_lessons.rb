acaed8c00be22bc909cf412689edd8e690f35c36
jasonoverby <disguisekit@gmail.com>
2016-06-03 12:41:23 -0700

    fixing migration

--- db/migrate/20160603135756_add_anticipated_to_lessons.rb
+++
@@ -1,5 +0,0 @@
-class AddAnticipatedToLessons < ActiveRecord::Migration
-  def change
-    add_column :lessons, :anticipated_problems, :text
-  end
-end
--- db/migrate/20160603135857_add_early_to_lessons.rb
+++
@@ -1,5 +0,0 @@
-class AddEarlyToLessons < ActiveRecord::Migration
-  def change
-    add_column :lessons, :early_finishers, :text
-  end
-end
