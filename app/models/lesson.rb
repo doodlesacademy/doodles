9347f3caa7e726f8d6c428fce8b8e1a497290366
jasonoverby <disguisekit@gmail.com>
2016-08-05 16:54:23 -0700

    changed button on last lesson of any project to go back to first lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -48,16 +48,15 @@ class Lesson < ActiveRecord::Base
     @video_uri.match(/\d+$/).to_s
   end
 
-  # def destroy
-  #   self.archive!
-  #   self.save!
-  # end
-
   def synopsis_old
     content = self.sections.first().content
     content = content[0, content.enum_for(:scan, /^## Objective/).map { Regexp.last_match.begin(0) }[0]]
     content = content.gsub(/#+ .*\n/, '')
     return content
+  end
+
+  def first_lesson
+    self.project.lessons.published.find_by(order: 0)
   end
 
   def next_lesson
