978a11bcb8e313262056113be425f055589e6bdb
Alex Hunley <ahunley@workwithopal.com>
2016-10-26 06:39:48 -0700

    :sparkles: adding last-viewed functionality to user and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016184745) do
+ActiveRecord::Schema.define(version: 20161026132313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,15 @@ ActiveRecord::Schema.define(version: 20161016184745) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "feedback_messages", force: :cascade do |t|
     t.text     "body"
@@ -340,12 +349,18 @@ ActiveRecord::Schema.define(version: 20161016184745) do
     t.datetime "updated_at",                          null: false
     t.integer  "status",                 default: 0
     t.integer  "role",                   default: 0
-    t.string   "last_name"
-    t.string   "first_name"
-    t.string   "gender"
+    t.integer  "last_lesson_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "video_bookmarks", force: :cascade do |t|
+    t.integer  "user_id",                    null: false
+    t.integer  "lesson_id",                  null: false
+    t.boolean  "completed",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
 end
