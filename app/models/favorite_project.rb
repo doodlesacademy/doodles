22496009c9b922c3515df8a6dfcb58e88be4d72c
jasonoverby <disguisekit@gmail.com>
2016-11-06 07:29:41 -0800

    fixed favoriting multiple projects

--- app/models/favorite_project.rb
+++ app/models/favorite_project.rb
@@ -1,5 +1,5 @@
 class FavoriteProject < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  validates_uniqueness_of :project_id
+  validates_uniqueness_of :project_id, scope: :user_id
 end
