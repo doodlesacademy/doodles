91053cc8d9506bcf8dccc17ec0962b1809f5046d
jasonoverby <disguisekit@gmail.com>
2016-11-24 06:48:35 -0800

    changed fields on project page

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -60,7 +60,7 @@ class Admin::ProjectsController < ApplicationController
         :second_photocopy, :second_photocopy_header, :second_photocopy_url, :third_photocopy, :third_photocopy_header,
         :fourth_photocopy, :fourth_photocopy_header, :fifth_photocopy, :fifth_photocopy_header, 
         :sixth_photocopy, :sixth_photocopy_header, :photocopy_copyright, :common_core, :national_core, 
-        :art_elements, :art_principles, :status, :cross_curricular, :inspiration_image, :featured_artist_image, 
+        :art_elements, :art_principles, :status, :core_content, :cross_curricular, :inspiration_image, :featured_artist_image, 
         :featured_artist_description, :engage_ny, :next_gen_sci,
         sections_attributes: [:id, :order, :content]
       )
