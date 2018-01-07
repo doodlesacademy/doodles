965640c051185c6dae2a19db10f64a858fa0462d
Alex Hunley <ahunley@nutshell.com>
2018-01-07 16:46:37 -0500

    :link: update donor/project-kit links

--- app/helpers/donors_helper.rb
+++ app/helpers/donors_helper.rb
@@ -1,5 +1,9 @@
 module DonorsHelper
   def donate_link
-    'https://squareup.com/store/doodles-academy'
+    'https://tempest5.wixsite.com/donations'
+  end
+
+  def project_kits_link
+    'https://tempest5.wixsite.com/donations/product-page/supply-kit-around-the-neighborhood'
   end
 end
