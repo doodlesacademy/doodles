7977a1d94f7cc192ed8b3a56cdddb2bc60344be4
jasonoverby <disguisekit@gmail.com>
2018-03-18 16:12:06 -0700

    cleaned up launch page index view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def lessons_include
+    ['Videos with demos', 'Downloadable Lesson Plans',
+     'Downloadable Supply Lists', 'Downloadable student handouts',
+     'Ideas for extending projects', 'Common Core & National Art Standards',
+     'Connections to open source curricula',
+     'Constantly expanding group of projects',
+     'Prompts for discussion', 'Suggestions for early finishers',
+     'Critical thinking skills in every lesson',
+     '"Knowledge building" topic based units']
+  end
 end
