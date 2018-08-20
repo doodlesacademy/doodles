896ca1c085a90f16872c92360ec5d39d5f1684ac
Alex Hunley <ahunley@nutshell.com>
2018-08-20 00:20:48 -0400

    :sparkles: checkout is looking better...

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
-  def formatted_price
+  def self.format_price(price)
     dollars = price / 100
     "$#{dollars}.00"
+  end
+
+  def formatted_price
+    self.class.format_price(price)
   end
 end
